a0efe3ce6dbef565768469959047db496dc4fe43
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-25 18:44:17 -0300

    challenge tests

--- script.js
+++ script.js
@@ -1,34 +1,32 @@
+//obs: código originalmente escrito pelo Professor Gabriel Oliva e modificado por mim, Robson Cardozo.
+
 let megaSenaDraw = [];
+let game = [04, 08, 15, 16, 23, 42];
 
-let firstNumber = Math.ceil(Math.random() * 60);
-let secondNumber = Math.ceil(Math.random() * 60);
-let thirdNumber = Math.ceil(Math.random() * 60);
-let fourthNumber = Math.ceil(Math.random() * 60);
-let fifthNumber = Math.ceil(Math.random() * 60);
-let sixthNumber = Math.ceil(Math.random() * 60);
+for (let i = 0; i < 6; i += 1) {
+  megaSenaDraw[i] = Math.ceil(Math.random() * 60);
+}
 
-megaSenaDraw.push(firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber);
-
-let michelPereiraGame = [7, 20, 45, 18, 32, 60];
+let megaSena = megaSenaDraw.sort();
 
 // Como conferir o jogo da mega?
 let numberOfHits = 0;
 
-for (let index = 0; index < megaSenaDraw.length; index += 1) {
-  let drawNumber = megaSenaDraw[index];
+//while (numberOfHits < 6) {
+  for (let j = 0; j < megaSena.length; j += 1) {
+    let drawNumber = megaSena[j];
+      for (let k = 0; k < game.length; k += 1) {
+        let gameNumber = game[k];
+        // gameNumber tem que ser IGUAL ao drawNumber
+        if (gameNumber === drawNumber) {
+         numberOfHits += 1;
+        }
+      }
+   }
+  //if (numberOfHits === 6) numberOfHits = 6;
+  //else numberOfHits = 0;
 
-  for (let gameIndex = 0; gameIndex < michelPereiraGame.length; gameIndex += 1) {
-    let gameNumber = michelPereiraGame[gameIndex];
 
-    // gameNumber tem que ser IGUAL ao drawNumber
-    if (gameNumber === drawNumber) {
-      numberOfHits += 1;
-    }
-
-  }
- }
-
-
-console.log('Sorteio da mega: ' + megaSenaDraw);
-console.log('Jogo do Michel: ' + michelPereiraGame);
+console.log('Sorteio da mega: ' + megaSena);
+console.log('Jogo: ' + game);
 console.log('O total de acertos foi: ' + numberOfHits);
