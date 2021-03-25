cbdd299cf384f2fdae5417272da605a966e24e33
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-25 19:23:53 -0300

    tests 1.2

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 //obs: código originalmente escrito pelo Professor Gabriel Oliva e modificado por mim, Robson Cardozo.
 
 let megaSenaDraw = [];
-let game = [04, 08, 15, 16, 23, 42];
+let game = [01, 05, 10, 12, 15, 20];
 
-for (let i = 0; i < 6; i += 1) {
-  megaSenaDraw[i] = Math.ceil(Math.random() * 60);
+function random() {
+  for (let i = 0; i < 6; i += 1) {
+    megaSenaDraw[i] = Math.ceil(Math.random() * 20);
+  }
+  megaSenaDraw = megaSenaDraw.sort();
+  return megaSenaDraw;
 }
-
-let megaSena = megaSenaDraw.sort();
 
 // Como conferir o jogo da mega?
 let numberOfHits = 0;
 
-//while (numberOfHits < 6) {
-  for (let j = 0; j < megaSena.length; j += 1) {
-    let drawNumber = megaSena[j];
+while (numberOfHits < 6) {
+  random();
+  for (let j = 0; j < megaSenaDraw.length; j += 1) {
+    let drawNumber = megaSenaDraw[j];
       for (let k = 0; k < game.length; k += 1) {
         let gameNumber = game[k];
         // gameNumber tem que ser IGUAL ao drawNumber
-        if (gameNumber === drawNumber) {
+      if (gameNumber === drawNumber) {
          numberOfHits += 1;
-        }
       }
-   }
-  //if (numberOfHits === 6) numberOfHits = 6;
-  //else numberOfHits = 0;
+    }
+  }
+  if (numberOfHits === 6) numberOfHits = 6;
+  else numberOfHits = 0;
+}
 
-
-console.log('Sorteio da mega: ' + megaSena);
+console.log('Sorteio da mega: ' + megaSenaDraw);
 console.log('Jogo: ' + game);
 console.log('O total de acertos foi: ' + numberOfHits);
